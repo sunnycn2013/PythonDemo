2dfcc1ad2a792ba5dc1c384939c0a1aa41708738
chaoyang.gcy <chaoyang.gcy@alibaba-inc.com>
2019-08-30 11:51:20 +0800

    test_github_streak

--- green.py
+++ green.py
@@ -23,8 +23,9 @@ def commit():
 def set_sys_time(year, month, day):
     # os.system('date -s %04d%02d%02d' % (year, month, day))
     sudoPassword = '123'
-    command = 'date %04d%02d%02d' % (year, month, day)
-    os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+    command = ('date -s %04d%02d%02d' % (year, month, day))
+    print(command)
+    # os.system('echo %s|sudo -S %s' % (sudoPassword, command))
 
 
 def trick_commit(year, month, day):
