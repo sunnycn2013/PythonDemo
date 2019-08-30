c60e7db4dc6f947c7395bc86618e483d0afb3cbc
chaoyang.gcy <chaoyang.gcy@alibaba-inc.com>
2019-08-30 11:52:53 +0800

    test_github_streak

--- green.py
+++ green.py
@@ -23,7 +23,8 @@ def commit():
 def set_sys_time(year, month, day):
     # os.system('date -s %04d%02d%02d' % (year, month, day))
     sudoPassword = '123'
-    command = ('date %04d%02d%02d' % (year, month, day))
+    # command = ('date %04d%02d%02d' % (year, month, day))
+    command = 'ls'
     os.system('echo %s|sudo -S %s' % (sudoPassword, command))
 
 
